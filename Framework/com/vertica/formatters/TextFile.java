74698e462a74bbc791bd6ba606d62a2bad77c0a8
William White <nilen234@gmail.com>
2013-12-06 02:07:29 -0500

    Added larger buffer for less I/O operations

--- Framework/com/vertica/formatters/TextFile.java
+++ Framework/com/vertica/formatters/TextFile.java
@@ -1,7 +1,6 @@
 package com.vertica.formatters;
 
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.lang.reflect.Array;
 
 public class TextFile implements IStorageFormatter{
@@ -11,7 +10,12 @@ public class TextFile implements IStorageFormatter{
 	
 	OutputStream storageTarget = null;
 	
+	byte[] outputBuffer;
+	int maxBufferSize, currentBufferSize;
+	
 	public void setupFormatWriter(IStorageTarget target){
+		
+		setupBuffer();
 		
 		try{
 			//Pass the type of file to be created to storage target and store the connection it creates
@@ -20,17 +24,66 @@ public class TextFile implements IStorageFormatter{
 
 	}
 	
-	public void writeToTarget(byte[] byteArray){
-		try{
-			//Write the byte array received from the data formatter to the storage target
-			storageTarget.write(byteArray);
-		}catch(Exception e){}
-    }
-	
 	public void closeFormatter(){
 		try{
+			//Write what's left in buffer
+			writeBufferContents();
 			//Close the storage target
 			storageTarget.close();
 		}catch(Exception e){}
 	}
+	
+    public void setupBuffer(){
+   	 	maxBufferSize = 6;
+        currentBufferSize = 0;
+        outputBuffer = new byte[maxBufferSize];
+   }
+   
+   public void resetBuffer(){
+	   	currentBufferSize = 0;
+	   	outputBuffer = new byte[maxBufferSize];
+   }
+   
+   public void writeToTarget(byte[] input){
+	   	int inputIndex = 0;
+	   	
+	   	for(int leftToWrite = Array.getLength(input); leftToWrite > 0;){
+	   		//Continues to loop while there are still bytes to write(in case input fills buffer several times)
+	       	if(leftToWrite <= maxBufferSize - currentBufferSize){
+	       		// Input will fit in buffer so copy it over
+	       		for(int j = 0; j < leftToWrite; j++){
+	       			outputBuffer[currentBufferSize] = input[inputIndex];
+	       			inputIndex++;
+	       			currentBufferSize++;
+	       		}
+	       		if(maxBufferSize == currentBufferSize){
+	       			// Check to make sure buffer didn't get full on the last byte
+	       			writeBufferContents();
+	       			resetBuffer();
+	       		}
+	       		leftToWrite = 0; //All bytes were written so set i to zero
+	       	}else{ // Input won't fit in buffer so only write part of input
+	       		
+	       		//Adjust how much will be left to write after this part is copied
+	       		leftToWrite = leftToWrite - (maxBufferSize - currentBufferSize);
+	       		//Input won't fit into buffer so only copy part of it
+	       		int runTo = maxBufferSize - currentBufferSize;
+	       		for(int j = 0; j < runTo; j++){
+	       			outputBuffer[currentBufferSize] = input[inputIndex];
+	       			inputIndex++;
+	       			currentBufferSize++;
+	       		}
+	       		
+	       		writeBufferContents();
+	       		resetBuffer();
+	       	}
+	   	}
+   }
+   
+   public void writeBufferContents(){
+	   try{
+			//Write the byte array received from the data formatter to the storage target
+			storageTarget.write(outputBuffer);
+		}catch(Exception e){}
+   }
 }
